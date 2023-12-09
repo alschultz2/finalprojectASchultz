635bb877050e305bd205f87105818b577bfbeb71
alschultz2 <alschultz2@mail.fhsu.edu>
2023-12-09 15:21:48 -0600

    Added a navagation bar so that I can later seperate the different parts of the project.

--- main.py
+++ main.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/register')
+def register():
+    return 'Register Page'
+
+@app.route('/login')
+def login():
+    return 'Login Page'
+
+@app.route('/tracker')
+def tracker():
+    return 'Time Tracker Page'
+
 if __name__ == '__main__':
     app.run(debug=True)

150580738a6d779b6f658cdd082d7a71cddd3c05
alschultz2 <alschultz2@mail.fhsu.edu>
2023-12-10 17:58:06 -0600

    Added the db.py, forms.py, and schema.sql files. I have also added the login and register functionality.

---
+++ forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, validators
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    password = PasswordField('Password', [validators.DataRequired()])
--- main.py
+++ main.py
@@ -1,22 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from db import get_db, close_db
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your-secret-key'
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/register')
+@app.route('/register', methods=('GET', 'POST'))
 def register():
-    return render_template('register.html')
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        username = form.username.data
+        password = generate_password_hash(form.password.data)
 
-@app.route('/login')
+        db = get_db()
+        db.execute('INSERT INTO user (username, password) VALUES (?, ?)', (username, password))
+        db.commit()
+        close_db()
+
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=('GET', 'POST'))
 def login():
-    return render_template('login.html')
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        username = form.username.data
+        password = form.password.data
 
-@app.route('/tracker')
-def tracker():
-    return render_template('tracker.html')
+        db = get_db()
+        user = db.execute('SELECT * FROM user WHERE username = ?', (username,)).fetchone()
+        close_db()
+
+        if user and check_password_hash(user['password'], password):
+            return redirect(url_for('home'))
+
+    return render_template('login.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)

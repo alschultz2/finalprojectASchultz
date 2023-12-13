a32bb1a1f9781f487586ba5d369f19950228cd70
alschultz2 <alschultz2@mail.fhsu.edu>
2023-12-12 18:56:31 -0600

    I have the login and register system actuall working. I didn't know that they didnt before. I also have the logout system working.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from db import get_db, close_db
 from forms import RegistrationForm, LoginForm
@@ -10,37 +10,31 @@ app.config['SECRET_KEY'] = 'your-secret-key'
 def home():
     return render_template('index.html')
 
-@app.route('/register', methods=('GET', 'POST'))
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    print("Register route accessed")  # Debug print
+    if 'user_id' in session:
+        return redirect(url_for('home'))
 
     form = RegistrationForm(request.form)
-    if request.method == 'POST':
-        if form.validate():
-            username = form.username.data
-            password = generate_password_hash(form.password.data)
+    if request.method == 'POST' and form.validate():
+        username = form.username.data
+        password = generate_password_hash(form.password.data)
 
-            print(f"Attempting to register user: {username}")  # Debug print
+        db = get_db()
+        db.execute('INSERT INTO user (username, password) VALUES (?, ?)', (username, password))
+        db.commit()
+        close_db()
 
-            try:
-                db = get_db()
-                db.execute('INSERT INTO user (username, password) VALUES (?, ?)', (username, password))
-                db.commit()
-
-                print(f"User {username} registered successfully")  # Debug print
-            except Exception as e:
-                print("An error occurred during registration:", e)  # Debug print
-            finally:
-                close_db()
-
-            return redirect(url_for('login'))
-        else:
-            print("Form validation failed")  # Debug print
+        flash('Registration successful. Please log in.')
+        return redirect(url_for('login'))
 
     return render_template('register.html', form=form)
 
-@app.route('/login', methods=('GET', 'POST'))
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if 'user_id' in session:
+        return redirect(url_for('home'))
+
     form = LoginForm(request.form)
     if request.method == 'POST' and form.validate():
         username = form.username.data
@@ -51,15 +45,35 @@ def login():
         close_db()
 
         if user and check_password_hash(user['password'], password):
+            session['user_id'] = user['id']
             return redirect(url_for('home'))
+        else:
+            flash('Invalid username or password.')
 
     return render_template('login.html', form=form)
 
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
+
+@app.route('/tracker')
+def tracker():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+
+    # Placeholder for time tracker page logic
+    return render_template('tracker.html')
+
 @app.route('/usernames')
 def show_usernames():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+
     db = get_db()
     usernames = db.execute('SELECT username FROM user').fetchall()
     close_db()
+
     return render_template('usernames.html', usernames=usernames)
 
 if __name__ == '__main__':

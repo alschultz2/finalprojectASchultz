0d76b1065412f2b5dac52f971685f64a142a246a
alschultz2 <alschultz2@mail.fhsu.edu>
2023-12-13 23:28:03 -0600

    Updated comments

--- forms.py
+++ forms.py
@@ -9,7 +9,9 @@ class RegistrationForm(FlaskForm):
     ])
     confirm = PasswordField('Repeat Password')
     manager_code = StringField('Manager Code (optional)')  # Add this line
+# Above is the registration form with the username, password, confirm password, and manager code field with the respective validators
 
 class LoginForm(FlaskForm):
     username = StringField('Username', [validators.Length(min=4, max=25)])
     password = PasswordField('Password', [validators.DataRequired()])
+# Above is the login form using flask, with the username and password fields with the respective validators
--- schema.sql
+++ schema.sql
@@ -7,3 +7,4 @@ CREATE TABLE IF NOT EXISTS time_entry (
     end_time TEXT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user (id)
 );
+-- Controls creating the table for the time entries, and for controlling the users information data
